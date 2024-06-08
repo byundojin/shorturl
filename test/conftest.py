69b8b7bda8b38fa69ed74168b87f5a86715863ec
byundojin <103907292+byundojin@users.noreply.github.com>
2024-06-08 15:48:58 +0900

    test 수정 오타 수정 exp -> url

--- test/conftest.py
+++ test/conftest.py
@@ -2,11 +2,11 @@ import pytest
 from db.redis_db import UrlRedis, ExpRedis
 
 @pytest.fixture
-def exp_db():
+def url_db():
     db = UrlRedis().get()
-    db.flushdb() # test 전 db 초기화
+    # db.flushdb() # test 전 db 초기화
     yield db
-    db.flushdb() # test 후 db 초기화
+    # db.flushdb() # test 후 db 초기화
 
 @pytest.fixture
 def exp_db():
--- test/db_test.py
+++ test/db_test.py
@@ -1,9 +1,9 @@
 import pytest 
 import redis
 
-def test_url_redis(exp_db:redis.Redis):
-    exp_db.set("A", "B")
-    assert exp_db.get("A") == "B".encode()
+def test_url_redis(url_db:redis.Redis):
+    url_db.set("A", "B")
+    assert url_db.get("A") == "B".encode()
 
 def test_exp_redis(exp_db:redis.Redis):
     exp_db.set("A", "B")
