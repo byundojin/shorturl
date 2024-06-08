658850229ef76d67d6921fab5dea478b9a980cf4
byundojin <103907292+byundojin@users.noreply.github.com>
2024-06-08 17:40:44 +0900

    조회수 조회 추가

--- main.py
+++ main.py
@@ -38,16 +38,19 @@ async def post_shorten(body:UrlBody):
     return JSONResponse({"short_url":id}, status_code=201)
 
 @app.get("/{short_key}")
-async def post_shorten(short_key:str):
+async def get_url(short_key:str):
     # url_db 조회
     url_db = UrlRedis().get()
     url:bytes = url_db.get(short_key)
 
+    if not url : # 조회 불가 시 404
+        return Response(status_code=404)
+    
     # view_db 조회
     view_db = ViewRedis().get()
     view:bytes = view_db.get(short_key)
 
-    if not url or not view: # 조회 불가 시 404
+    if not view: # 조회 불가 시 404
         return Response(status_code=404)
     
     url = url.decode()
@@ -71,3 +74,37 @@ async def post_shorten(short_key:str):
     view_db.set(short_key, view + 1)
 
     return RedirectResponse(url, status_code=301)
+
+@app.get("/stats/{short_key}")
+async def get_stat(short_key:str):
+    # url_db 조회
+    url_db = UrlRedis().get()
+    url:bytes = url_db.get(short_key)
+
+    if not url : # 조회 불가 시 404
+        return Response(status_code=404)
+    url = url.decode()
+
+    # view_db 조회
+    view_db = ViewRedis().get()
+    view:bytes = view_db.get(short_key)
+
+    if not view: # 조회 불가 시 404
+        return Response(status_code=404)
+    view = int(view)
+
+    # 만료 기간 확인
+    exp_db = ExpRedis().get()
+    exp:bytes = exp_db.get(short_key)
+    if exp:
+        exp = exp.decode()
+        now = datetime.datetime.now()
+        exp_time = datetime.datetime.fromisoformat(exp)
+
+        if now > exp_time: # 기간 만료
+            url_db.delete(short_key)
+            exp_db.delete(short_key)
+            view_db.delete(short_key)
+            return JSONResponse({"detail":"url 기간 만료"}, status_code=410)
+        
+    return JSONResponse({"views": view}, status_code=200)
